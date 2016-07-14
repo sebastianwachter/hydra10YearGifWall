ac6a448a7b8f326928cf88a9aa0cda397e546c59
Sebastian Wachter <wachter@hydra-newmedia.com>
2016-07-14 11:53:10 +0200

    grid wall implemantation

--- Client/source/javascript/main.js
+++ Client/source/javascript/main.js
@@ -1,1 +1,11 @@
-console.log('grainer');
+$('.item').each(function () {
+    let width = Math.floor(Math.random() * (600 - 150)) + 150;
+    let height = Math.floor(Math.random() * (600 - 150)) + 150;
+
+    //$(this).append('<img src="http://lorempixel.com/' + width + '/' + height + '">');
+
+    $(this).css({
+        'width': width,
+        'height': height
+    });
+});
