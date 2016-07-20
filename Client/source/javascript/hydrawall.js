bfb3c14718444c897d578927cf246f790f7dcd5a
Stéphan Barbé <barbe@hydra-newmedia.com>
2016-07-20 15:42:04 +0200

    hydrawall

--- Client/source/javascript/hydrawall.js
+++ Client/source/javascript/hydrawall.js
@@ -20,22 +20,3 @@ Hydrawall.prototype.setPoints = function () {
 
 
 }
-
-/*
-$('#contianer').css({
-    'width': $(document).width(),
-    'height': $(document).height()
-});
-
-$('.item').each(function () {
-    let width = Math.floor(Math.random() * (600 - 150)) + 150;
-    let height = Math.floor(Math.random() * (600 - 150)) + 150;
-
-    //$(this).append('<img src="http://lorempixel.com/' + width + '/' + height + '">');
-
-    $(this).css({
-        'width': width,
-        'height': height
-    });
-});
-*/
